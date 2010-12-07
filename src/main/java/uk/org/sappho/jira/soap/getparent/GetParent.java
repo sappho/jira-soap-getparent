675c4d39788f8fc62af925306905c1e027811f12
Andrew Heald <andrew@heald.co.uk>
2010-12-07 16:59:56 +0000

    CHM-32: Add simple authentication.

--- src/main/java/uk/org/sappho/jira/soap/getparent/GetParent.java
+++ src/main/java/uk/org/sappho/jira/soap/getparent/GetParent.java
@@ -1,6 +1,10 @@
 package uk.org.sappho.jira.soap.getparent;
 
+import com.atlassian.jira.rpc.exception.RemoteException;
+
 public interface GetParent {
 
-    public String getParentKey(String issueKey);
+    public String login(String username, String password) throws RemoteException;
+
+    public String getParentKey(String token, String issueKey) throws RemoteException;
 }
--- src/main/java/uk/org/sappho/jira/soap/getparent/GetParentImpl.java
+++ src/main/java/uk/org/sappho/jira/soap/getparent/GetParentImpl.java
@@ -3,18 +3,28 @@ package uk.org.sappho.jira.soap.getparent;
 import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.issue.IssueManager;
 import com.atlassian.jira.issue.MutableIssue;
+import com.atlassian.jira.rpc.auth.TokenManager;
+import com.atlassian.jira.rpc.exception.RemoteException;
 
 public class GetParentImpl implements GetParent {
 
+    private final TokenManager tokenManager;
     private final IssueManager issueManager;
 
-    public GetParentImpl(IssueManager issueManager) {
+    public GetParentImpl(TokenManager tokenManager, IssueManager issueManager) {
 
+        this.tokenManager = tokenManager;
         this.issueManager = issueManager;
     }
 
-    public String getParentKey(String issueKey) {
+    public String login(String username, String password) throws RemoteException {
 
+        return tokenManager.login(username, password);
+    }
+
+    public String getParentKey(String token, String issueKey) throws RemoteException {
+
+        tokenManager.retrieveUserNoPermissionCheck(token);
         MutableIssue issue = issueManager.getIssueObject(issueKey);
         Issue parentIssue = issue.getParentObject();
         return parentIssue != null ? parentIssue.getKey() : null;
